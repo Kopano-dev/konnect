92dfffe54915930226b1c848f05e5c4fa6ddfde0
Simon Eisenmann <s.eisenmann@kopano.com>
2018-10-30 17:26:37 +0100

    Add a bunch of languages for translation

--- identifier/src/locales/index.js
+++ identifier/src/locales/index.js
@@ -3,7 +3,26 @@
 
 // Please keep imports and exports alphabetically sorted.
 import de from './de.json';
+import es from './es.json';
+import fr from './fr.json';
+import hu from './hu.json';
+import it from './it.json';
+import ja from './ja.json';
+import nb from './nb.json';
+import nl from './nl.json';
+import ptPT from './pt_PT.json';
+import ru from './ru.json';
 
 export default {
-  de
+  de,
+  es,
+  fr,
+  hu,
+  it,
+  ja,
+  nb,
+  nl,
+  pt: ptPT,
+  'pt-pt': ptPT,
+  ru
 };
