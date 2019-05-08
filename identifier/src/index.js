73e6c03a1d9ef89f9cb469c21ebde2893eae20b1
Simon Eisenmann <s.eisenmann@kopano.com>
2019-05-08 16:07:29 +0200

    Update kpop and react-scripts

--- identifier/src/index.js
+++ identifier/src/index.js
@@ -1,11 +1,12 @@
-import 'kpop/es/styles/kpop';
+import 'kpop/static/css/base.css';
+import 'kpop/static/css/scrollbar.css';
 import 'typeface-roboto';
 import './app.css';
+import './fancy-background.css';
 
 import * as kpop from 'kpop/es/version';
 
 import * as version from './version';
-import './fancy-background.css';
 
 console.info(`Kopano Identifier build version: ${version.build}`); // eslint-disable-line no-console
 console.info(`Kopano Kpop build version: ${kpop.build}`); // eslint-disable-line no-console
